3794735338a0263351f4016560132f051e4a33b5
Yanghan Wang <yanghan@fb.com>
2021-06-22 18:11:07 -0700

    easier way to separate internal code from oss
    Summary: Pull Request resolved: https://github.com/facebookresearch/d2go/pull/90
    
    Reviewed By: zhanghang1989
    
    Differential Revision: D29279123
    
    fbshipit-source-id: d94cea65bd439d54fd14afded0dba066799cedca

--- d2go/data/__init__.py
+++ d2go/data/__init__.py
@@ -1,5 +1,2 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-
-import d2go.data.datasets
--- d2go/utils/launch_environment.py
+++ d2go/utils/launch_environment.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from d2go.utils.misc import fb_overwritable
+
+
+@fb_overwritable()
 def get_launch_environment():
     return "local"
