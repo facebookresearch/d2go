274d3b49faf2bedf41c87d7e9b77e570e84deb07
Yanghan Wang <yanghan@fb.com>
2021-10-20 08:53:40 -0700

    use fb. in import path - modeling/backbone/fb
    Summary: see bottom diff
    
    Reviewed By: newstzpz
    
    Differential Revision: D31780235
    
    fbshipit-source-id: ec1285c4c5457a631e1eb88bebd47c9f41b47e12

---
+++ d2go/modeling/backbone/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from . import fbnet_v2
+
+
+# Explicitly expose all registry-based modules
+__all__ = [
+    "fbnet_v2",
+]
