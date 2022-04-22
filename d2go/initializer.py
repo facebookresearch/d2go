68516a38e208986a7fdfec43f8c5537ac88e29b5
Yanghan Wang <yanghan@fb.com>
2022-04-22 15:00:15 -0700

    remove the TestStartupTime
    Differential Revision: D35853885
    
    fbshipit-source-id: 0ad2951042b75024dca64cc5207757e128a93b1f

--- d2go/initializer.py
+++ d2go/initializer.py
@@ -2,28 +2,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 
-import time
-
-
-SETUP_ENV_TIME = []
-REGISTER_D2_DATASETS_TIME = []
-REGISTER_TIME = []
-
-
-def _record_times(time_list):
-    def warp(f):
-        def timed_f(*args, **kwargs):
-            start = time.perf_counter()
-            ret = f(*args, **kwargs)
-            time_list.append(time.perf_counter() - start)
-            return ret
-
-        return timed_f
-
-    return warp
-
-
-@_record_times(SETUP_ENV_TIME)
 def _setup_env():
     # Set up custom environment before nearly anything else is imported
     # NOTE: this should be the first import (no not reorder)
@@ -32,13 +10,11 @@ def _setup_env():
     )
 
 
-@_record_times(REGISTER_D2_DATASETS_TIME)
 def _register_d2_datasets():
     # this will register D2 builtin datasets
     import detectron2.data  # noqa F401
 
 
-@_record_times(REGISTER_TIME)
 def _register():
     from d2go.data import dataset_mappers  # NOQA
     from d2go.data.datasets import (
