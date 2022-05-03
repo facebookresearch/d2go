a9016d1c06fb713dc06029160dec98b6fb77be3c
Miquel Jubert <miquel.hermoso@oculus.com>
2022-05-03 08:15:18 -0700

    Refactor dataset.py to be buck-friendly
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/d2go/pull/231
    
    *This diff is part of a stack which has the goal of "buckifying" D2 (https://github.com/facebookresearch/d2go/commit/87374efb134e539090e0b5c476809dc35bf6aedb)Go core and enabling autodeps and other tooling. The last diff in the stack introduces the TARGETS. The diffs earlier in the stack are resolving circular dependencies and other issues which prevent the buckification from occurring.*
    
    Similar strategy as applied before. A package is created based around __init__.py. Anyone importing from the package will receive either the fb or oss version of the function transparently. To get a specific version of the functions the import needs to be done from the corresponding file directly.
    
    This will lets me split the dependency into three modules, and import from the one I want directly to avoid circular import issues.
    
    Differential Revision: https://www.internalfb.com/diff/D35964998?entry_point=27
    
    fbshipit-source-id: f9bcd483c9068cc75c5607915b94bfc875506111

--- d2go/data/__init__.py
+++ d2go/data/__init__.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from .datasets import _register_extended_coco # @oss-only
+from .fb.datasets import _register_extended_coco  # @fb-only # noqa
