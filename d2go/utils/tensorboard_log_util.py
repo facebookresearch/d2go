159115d88b267feeb221d38bbf22b74c6d49980e
Miquel Jubert Hermoso <jmiquel@fb.com>
2022-05-19 08:47:41 -0700

    Move tensorboard_log_util to OSS pattern 2
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/d2go/pull/257
    
    WTTS
    
    Reviewed By: wat3rBro
    
    Differential Revision: D36438059
    
    fbshipit-source-id: 010cfef86795dc65ed279124c1ed32206829fdaa

--- d2go/utils/tensorboard_log_util.py
+++ d2go/utils/tensorboard_log_util.py
@@ -6,6 +6,9 @@ import logging
 import os
 from functools import lru_cache
 
+from d2go.utils.oss_helper import fb_overwritable
 
+
+@fb_overwritable()
 def get_tensorboard_log_dir(output_dir):
     return output_dir
