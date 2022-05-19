29f4f8c13021fbdccbb4de3d1e1f24d9fe0696bc
Miquel Jubert Hermoso <jmiquel@fb.com>
2022-05-19 08:47:41 -0700

    Change launch_environment to pattern 2
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/d2go/pull/253
    
    WTTS
    
    Reviewed By: wat3rBro
    
    Differential Revision: D36438058
    
    fbshipit-source-id: 1909cd31481eca92474baf952bd227256c786ac2

--- d2go/utils/launch_environment.py
+++ d2go/utils/launch_environment.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+from d2go.utils.oss_helper import fb_overwritable
 
-MODEL_ZOO_STORAGE_PREFIX = "https://mobile-cv.s3-us-west-2.amazonaws.com/d2go/models/"
+
+@fb_overwritable()
+def get_model_zoo_storage_prefix() -> str:
+    return "https://mobile-cv.s3-us-west-2.amazonaws.com/d2go/models/"
 
 
+@fb_overwritable()
 def get_launch_environment():
     return "local"
+
+
+MODEL_ZOO_STORAGE_PREFIX = get_model_zoo_storage_prefix()
