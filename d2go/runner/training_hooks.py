8f6ac56d891f82bccd9e62ff716d802ff4efa90d
Francisc Bungiu <frabu@meta.com>
2022-10-03 03:16:34 -0700

    enable zoomer for train_net
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/d2go/pull/378
    
    Some hooks need access to cfg to be initialized correctly. Pass cfg down the hook registration method.
    
    Reviewed By: ertrue, miqueljubert
    
    Differential Revision: D39303862
    
    fbshipit-source-id: 339b09da9f8da3d1412afb92cf735b60a71345aa

--- d2go/runner/training_hooks.py
+++ d2go/runner/training_hooks.py
@@ -3,8 +3,11 @@
 import logging
 from typing import List
 
+from d2go.config import CfgNode
+
 from detectron2.engine import HookBase
 from detectron2.utils.registry import Registry
+
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +17,7 @@ logger = logging.getLogger(__name__)
 TRAINER_HOOKS_REGISTRY = Registry("TRAINER_HOOKS_REGISTRY")
 
 
-def update_hooks_from_registry(hooks: List[HookBase]):
+def update_hooks_from_registry(hooks: List[HookBase], cfg: CfgNode):
     for name, hook_func in TRAINER_HOOKS_REGISTRY:
         logger.info(f"Update trainer hooks from {name}...")
-        hook_func(hooks)
+        hook_func(hooks, cfg)
