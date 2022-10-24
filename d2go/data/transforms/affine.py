add unit test for affine augmentation, fix off by one
Summary:
Pull Request resolved: https://github.com/facebookresearch/d2go/pull/391

1. Add unit test for affine augmentation
2. Fix off by one affine scaling (Note: this changes augmentation behavior)

Reviewed By: tglik

Differential Revision: D40374538

fbshipit-source-id: 42d798d4a40c7ed37a20bba798b5e6300e0ea317

d2go/data/transforms/affine.py
@@ -161,8 +161,8 @@ class RandomAffine(TransformGen):
         # Center in output patch
         img_corners = np.array(
             [
-                [0, 0, im_w, im_w],
-                [0, im_h, 0, im_h],
+                [0, 0, im_w - 1, im_w - 1],
+                [0, im_h - 1, 0, im_h - 1],
                 [1, 1, 1, 1],
             ]
         )