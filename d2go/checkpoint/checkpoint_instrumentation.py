8bf82728c7f1c27c050f817c9770c6781a6ac5e8
John Lee <johnjlee@meta.com>
2023-05-07 13:44:20 -0700

    Instrument checkpoints for FSDPCheckpointer
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/d2go/pull/536
    
    This diff insteuments checkpoints using signpost for FSDPCheckpointer using D44278485 as a reference
    
    Reviewed By: miqueljubert
    
    Differential Revision: D45524792
    
    fbshipit-source-id: 14616426a740b1efe70db6f1e32611f83d121d57

---
+++ d2go/checkpoint/checkpoint_instrumentation.py
@@ -0,0 +1,36 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import logging
+import uuid
+from contextlib import ContextDecorator
+
+from d2go.checkpoint.log_checkpoint import log_checkpoint
+
+
+logger = logging.getLogger(__name__)
+
+
+class instrument_checkpoint(ContextDecorator):
+    def __init__(
+        self,
+        checkpoint_type: str,
+    ) -> None:
+        super().__init__()
+        self.unique_id = uuid.uuid1().int >> 97
+        self.checkpoint_type = checkpoint_type
+
+    def __enter__(self) -> "instrument_checkpoint":
+        log_checkpoint(
+            checkpoint_type=self.checkpoint_type,
+            unique_id=self.unique_id,
+            state="begin",
+        )
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb) -> bool:
+        log_checkpoint(
+            checkpoint_type=self.checkpoint_type,
+            unique_id=self.unique_id,
+            state="end",
+        )
+        return True
