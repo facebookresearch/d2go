0389f4eed6ea340ce48b7e23c829b24f61a5201f
Anthony Chen <yanjunchen@meta.com>
2023-06-14 04:00:33 -0700

    Enable activation checkpointing
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/d2go/pull/573
    
    Enable Activation Checkpointing from Pytorch Distributed in d2go.
    
    Reviewed By: rohan-varma
    
    Differential Revision: D45681009
    
    fbshipit-source-id: c03f27af61e0374b9e5991d82070edbe41edde6d

--- d2go/trainer/fsdp.py
+++ d2go/trainer/fsdp.py
@@ -3,16 +3,14 @@
 import contextlib
 import logging
 from enum import Enum
-from functools import partial
-from typing import Callable, Generator, Iterable, Optional
+from typing import Generator, Optional
 
 import torch
 import torch.nn as nn
 from d2go.config import CfgNode as CN
 from d2go.modeling.modeling_hook import ModelingHook
 from d2go.registry.builtin import MODELING_HOOK_REGISTRY
-from d2go.trainer.helper import parse_precision_from_string
-from detectron2.utils.registry import Registry
+from d2go.trainer.helper import D2GO_WRAP_POLICY_REGISTRY, parse_precision_from_string
 from torch.ao.pruning import fqn_to_module
 from torch.cuda.amp import GradScaler
 from torch.distributed.fsdp.fully_sharded_data_parallel import (
@@ -27,16 +25,9 @@ from torch.distributed.fsdp.fully_sharded_data_parallel import (
     StateDictType,
 )
 from torch.distributed.fsdp.sharded_grad_scaler import ShardedGradScaler
-from torch.distributed.fsdp.wrap import (
-    always_wrap_policy as _always_wrap_policy,
-    size_based_auto_wrap_policy as _size_based_auto_wrap_policy,
-    transformer_auto_wrap_policy as _layer_based_auto_wrap_policy,
-)
 
 
 logger = logging.getLogger(__name__)
-
-D2GO_FSDP_WRAP_POLICY_REGISTRY = Registry("D2GO_FSDP_WRAP_POLICY_REGISTRY")
 
 
 def add_fsdp_configs(_C: CN):
@@ -49,7 +40,7 @@ def add_fsdp_configs(_C: CN):
     # See docstring of CpuOffload and BackwardPrefetch in torch.distributed.fsdp.fully_sharded_data_parallel
     _C.FSDP.CPU_OFFLOAD = False
     _C.FSDP.BACKWARD_PREFETCH = True
-    # Find autowrap policy at D2GO_FSDP_WRAP_POLICY_REGISTRY, or use '' to disable autowrap
+    # Find autowrap policy at D2GO_WRAP_POLICY_REGISTRY, or use '' to disable autowrap
     _C.FSDP.AUTO_WRAP_POLICY = "never_wrap_policy"
     _C.FSDP.AUTO_WRAP_MIN_PARAMS = int(1e4)
     # A list of layer cls names to wrap, case sensitive
@@ -210,7 +201,7 @@ def build_fsdp(
         )
 
     auto_wrap_policy = (
-        D2GO_FSDP_WRAP_POLICY_REGISTRY.get(auto_wrap_policy_name)(
+        D2GO_WRAP_POLICY_REGISTRY.get(auto_wrap_policy_name)(
             model, **auto_wrap_policy_kwargs
         )
         if auto_wrap_policy_name != ""
@@ -321,83 +312,3 @@ class FSDPModelingHook(ModelingHook):
         raise NotImplementedError(
             "FSDPModelingHook.unapply() not implemented: can't unwrap a FSDP module"
         )
-
-
-def get_module_class_from_name(module, name):
-    """
-    Gets a class from a module by its name. Code borrowed from HuggingFace
-    Args:
-        module (`torch.nn.Module`): The module to get the class from.
-        name (`str`): The name of the class.
-    """
-    modules_children = list(module.children())
-    if module.__class__.__name__ == name:
-        return module.__class__
-    elif len(modules_children) == 0:
-        return
-    else:
-        for child_module in modules_children:
-            module_class = get_module_class_from_name(child_module, name)
-            if module_class is not None:
-                return module_class
-
-
-@D2GO_FSDP_WRAP_POLICY_REGISTRY.register()
-def never_wrap_policy(model, **kwargs) -> Optional[Callable]:
-    """
-    Don't wrap any child module, only wrap the root
-    """
-
-    def never_wrap(*args, **kwargs):
-        return False
-
-    return never_wrap
-
-
-@D2GO_FSDP_WRAP_POLICY_REGISTRY.register()
-def always_wrap_policy(model, **kwargs) -> Optional[Callable]:
-    """
-    Wrapper for always_wrap_policy() from torch.distributed.fsdp.wrap
-    """
-    return _always_wrap_policy
-
-
-@D2GO_FSDP_WRAP_POLICY_REGISTRY.register()
-def size_based_auto_wrap_policy(
-    model, min_num_params=1e4, **kwargs
-) -> Optional[Callable]:
-    """
-    Wrapper for size_based_auto_wrap_policy() from torch.distributed.fsdp.wrap
-    """
-    # Note: be careful when using auto wrap with shared parameters.
-    # Errors will be thrown if shared parameters reside in different FSDP units
-    return partial(
-        _size_based_auto_wrap_policy,
-        min_num_params=min_num_params,
-    )
-
-
-@D2GO_FSDP_WRAP_POLICY_REGISTRY.register()
-def layer_based_auto_wrap_policy(
-    model, layer_names: Iterable[str], **kwargs
-) -> Optional[Callable]:
-    """
-    Wrapper for transformer_auto_wrap_policy() from torch.distributed.fsdp.wrap
-    Args:
-        layer_names: a list of layer names
-    """
-    assert (
-        len(layer_names) > 0
-    ), "FSDP.AUTO_WRAP_LAYER_CLS should be a nonempty list of layer names contained in the model"
-    layer_cls = []
-    for name in layer_names:
-        closure = get_module_class_from_name(model, name)
-        if closure is None:
-            raise Exception(
-                f"Could not find the layer class {name} to wrap in the model."
-            )
-        layer_cls.append(closure)
-    return partial(
-        _layer_based_auto_wrap_policy,
-        transformer_layer_cls=layer_cls,
-    )
