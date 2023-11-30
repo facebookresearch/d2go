d0e16684175334ca569685ce23d81c8802a77e23
Yanghan Wang <yanghan@meta.com>
2023-11-30 12:11:28 -0800

    add callbacks for inference_on_dataset
    Summary: Pull Request resolved: https://github.com/facebookresearch/d2go/pull/637
    
    Reviewed By: tglik
    
    Differential Revision: D51540498
    
    fbshipit-source-id: f246559963c5187140db7b8113765f66a964ae1b

--- d2go/evaluation/evaluator.py
+++ d2go/evaluation/evaluator.py
@@ -36,6 +36,7 @@ def inference_on_dataset(
     model: torch.nn.Module,
     data_loader: Iterable,
     evaluator: Union[DatasetEvaluator, List[DatasetEvaluator], None],
+    **kwargs,
 ):
     """
     A drop-in replacement for d2's inference_on_dataset to run inference on datasets,
@@ -43,7 +44,7 @@ def inference_on_dataset(
     * has_finished_process(self) -> bool: return True if `self.process()` could be skipped
     """
     if evaluator is None:
-        return inference_on_dataset_d2(model, data_loader, evaluator)
+        return inference_on_dataset_d2(model, data_loader, evaluator, **kwargs)
 
     if isinstance(evaluator, abc.MutableSequence):
         evaluator = DatasetEvaluators(evaluator)
@@ -51,7 +52,7 @@ def inference_on_dataset(
     if not (
         hasattr(evaluator, "has_finished_process") and evaluator.has_finished_process()
     ):
-        return inference_on_dataset_d2(model, data_loader, evaluator)
+        return inference_on_dataset_d2(model, data_loader, evaluator, **kwargs)
 
     evaluator.reset()
     results = evaluator.evaluate()
