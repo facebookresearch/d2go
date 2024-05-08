ed952120c1da2ab354290110bd329127251f9441
Yanghan Wang <yanghan@meta.com>
2024-05-07 20:16:46 -0700

    small fixes of github CI
    Summary:
    
    - d2go: aws model zoo is not available anymore, disable the test in oss.
    - d2: scripts running at midnight hitting rate limit issue, change it to a random time.
    
    Differential Revision: D57085427

--- tests/modeling/test_model_zoo.py
+++ tests/modeling/test_model_zoo.py
@@ -5,9 +5,11 @@ import unittest
 
 import torch.nn as nn
 from d2go.model_zoo import model_zoo
+from mobile_cv.common.misc.oss_utils import is_oss
 
 
 class TestD2GoModelZoo(unittest.TestCase):
+    @unittest.skipIf(is_oss(), "Model Zoo is not available in OSS")
     def test_model_zoo_pretrained(self):
         configs = list(model_zoo._ModelZooUrls.CONFIG_PATH_TO_URL_SUFFIX.keys())
         for cfgfile in configs:
